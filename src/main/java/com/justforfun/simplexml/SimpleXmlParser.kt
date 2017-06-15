428eb01c559ee55e1b9124ada54dcd8364ae168a
Vladimir <vladymyr.malykov@gmail.com>
2017-06-15 18:35:37 +0300

    Add class public API

---
+++ src/main/java/com/justforfun/simplexml/SimpleXmlParser.kt
@@ -0,0 +1,15 @@
+package com.justforfun.simplexml
+
+import com.justforfun.simplexml.core.SimpleParser
+import java.io.InputStream
+
+/**
+ * Created by Vladimir on 6/15/17.
+ */
+class SimpleXmlParser {
+    companion object {
+        fun <T> parse(input: InputStream?, clazz: Class<T>): T? {
+            return SimpleParser().parse<T>(input, clazz)
+        }
+    }
+}
