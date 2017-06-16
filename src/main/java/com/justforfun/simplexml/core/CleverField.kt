eb1aa7974868b143dd669fe53900c51d71a4177f
Vladimir <vladymyr.malykov@gmail.com>
2017-06-16 17:27:18 +0300

    Improve isPrimitive method

--- src/main/java/com/justforfun/simplexml/core/CleverField.kt
+++ src/main/java/com/justforfun/simplexml/core/CleverField.kt
@@ -19,7 +19,31 @@ class CleverField(var field: Field, var as_array: Boolean) {
         }
     }
 
-    fun isPrimitive(): Boolean = type().isPrimitive || type() == String::class.java
+    fun isPrimitive(): Boolean {
+        return type().isPrimitive
+                || type() == String::class.java
+
+                || type() == Byte::class.java
+                || type() == java.lang.Byte::class.java
+
+                || type() == Short::class.java
+                || type() == java.lang.Short::class.java
+
+                || type() == Integer::class.java
+                || type() == Int::class.java
+
+                || type() == Long::class.java
+                || type() == java.lang.Long::class.java
+
+                || type() == Float::class.java
+                || type() == java.lang.Float::class.java
+
+                || type() == Double::class.java
+                || type() == java.lang.Double::class.java
+
+                || type() == Boolean::class.java
+                || type() == java.lang.Boolean::class.java
+    }
 
     fun type(): Class<*> {
         return if (as_array) getGenericType() else field.type
