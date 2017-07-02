9c56d68855fa7eb0d57b64eaa89d84bb6b9640ac
Vladimir <vladymyr.malykov@gmail.com>
2017-07-02 12:17:07 +0300

    Add ability to mark data class constructor params with parsing annotation

--- src/main/java/com/justforfun/simplexml/annotation/XmlName.kt
+++ src/main/java/com/justforfun/simplexml/annotation/XmlName.kt
@@ -4,7 +4,7 @@ package com.justforfun.simplexml.annotation
  * Created by Vladimir on 6/7/17.
  */
 
-@Target(AnnotationTarget.CLASS, AnnotationTarget.FIELD)
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FIELD, AnnotationTarget.VALUE_PARAMETER)
 @Retention(AnnotationRetention.RUNTIME)
 @MustBeDocumented
 annotation class XmlName(val name: String = "", vararg val names: String = arrayOf<String>())
